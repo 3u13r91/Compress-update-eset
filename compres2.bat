e0ee9f3ff94c4b3baf3ba97890f600c6cf4a3bc9
3u13r91 <64043883+3u13r91@users.noreply.github.com>
2024-03-17 16:27:19 -0400

    Update compres2.bat

--- compres2.bat
+++ compres2.bat
@@ -1,132 +1,36 @@
-using System;
-using System.Diagnostics;
-using System.IO;
-using System.Windows.Forms;
+@echo off
+Title Actualización de Eset 2.0
+color 0A
 
-namespace ActualizadorEset
-{
-    public class MainForm : Form
-    {
-        private Button btnActualizar;
-        private TextBox txtOutput;
-        private string sevenZipPath = @".\7z\";
-        private string sfxPath = @".\sfx\";
-        private string destinationPath = @"D:\APLICACIONES\ANTIVIRUS\ESET\Actualizacion\";
+:: Sección para eliminar archivos antiguos
+echo ------------------------------------------------------------
+echo Eliminando archivos antiguos...
+if exist ".\7z\nod.7z" del /Q ".\7z\nod.7z"
+if exist ".\sfx\*.exe" del /Q ".\sfx\*.exe"
 
-        public MainForm()
-        {
-            this.btnActualizar = new Button
-            {
-                Text = "Actualizar Eset",
-                Location = new System.Drawing.Point(10, 10),
-                Size = new System.Drawing.Size(100, 30)
-            };
-            this.btnActualizar.Click += new EventHandler(this.BtnActualizar_Click);
+:: Sección para cambiar de directorio
+echo ------------------------------------------------------------
+echo Cambiando directorio a "..\7z"...
+cd /d ".\7z"
 
-            this.txtOutput = new TextBox
-            {
-                Location = new System.Drawing.Point(10, 50),
-                Size = new System.Drawing.Size(280, 200),
-                Multiline = true,
-                ScrollBars = ScrollBars.Vertical,
-                ReadOnly = true
-            };
+:: Sección para comprimir archivos
+echo ------------------------------------------------------------
+echo Comprimiendo archivos a "nod.7z"...
+7z a -t7z "nod.7z" @source.txt -r -mx0
 
-            this.Controls.Add(this.btnActualizar);
-            this.Controls.Add(this.txtOutput);
-        }
+:: Sección para crear ejecutables
+echo ------------------------------------------------------------
+echo Creando ejecutables en "..\sfx"...
+copy /b "7zsd_All.sfx" + "script.txt" + "nod.7z" "..\sfx\eset_update.exe"
+copy /b "7zsd_All.sfx" + "script_m.txt" + "nod.7z" "..\sfx\eset_update_m.exe"
 
-        private void BtnActualizar_Click(object sender, EventArgs e)
-        {
-            try
-            {
-                // Eliminar archivos antiguos
-                EliminarArchivos(sevenZipPath + "nod.7z");
-                EliminarArchivos(sfxPath + "*.exe");
+:: Sección para copiar a la carpeta de destino
+echo ------------------------------------------------------------
+echo Copiando a la carpeta de destino...
+xcopy /y /c /f "..\sfx\eset_update.exe" "D:\APLICACIONES\ANTIVIRUS\ESET\Actualizacion\eset_update.exe"
 
-                // Comprimir archivos
-                ComprimirArchivos(sevenZipPath, "nod.7z", "@source.txt");
-
-                // Crear ejecutables
-                CrearEjecutables(sevenZipPath, sfxPath, "nod.7z", "eset_update.exe", "script.txt");
-                CrearEjecutables(sevenZipPath, sfxPath, "nod.7z", "eset_update_m.exe", "script_m.txt");
-
-                // Copiar a la carpeta de destino
-                CopiarADestino(sfxPath + "eset_update.exe", destinationPath + "eset_update.exe");
-
-                txtOutput.AppendText("Proceso completado.\r\n");
-            }
-            catch (Exception ex)
-            {
-                txtOutput.AppendText("Error: " + ex.Message + "\r\n");
-            }
-        }
-
-        private void EliminarArchivos(string path)
-        {
-            if (File.Exists(path))
-            {
-                File.Delete(path);
-                txtOutput.AppendText("Eliminado: " + path + "\r\n");
-            }
-        }
-
-        private void ComprimirArchivos(string directory, string archiveName, string fileList)
-        {
-            ProcessStartInfo psi = new ProcessStartInfo
-            {
-                FileName = "7z.exe",
-                Arguments = $"a -t7z \"{archiveName}\" {fileList} -r -mx0",
-                WorkingDirectory = directory,
-                CreateNoWindow = true,
-                UseShellExecute = false,
-                RedirectStandardOutput = true
-            };
-
-            using (Process p = Process.Start(psi))
-            {
-                p.WaitForExit();
-                txtOutput.AppendText(p.StandardOutput.ReadToEnd());
-            }
-        }
-
-        private void CrearEjecutables(string sevenZipPath, string sfxPath, string archiveName, string executableName, string scriptName)
-        {
-            string command = $"/C copy /b \"7zsd_All.sfx\" + \"{scriptName}\" + \"{archiveName}\" \"{sfxPath}{executableName}\"";
-            ExecuteCommand(command, sevenZipPath);
-        }
-
-        private void CopiarADestino(string source, string destination)
-        {
-            File.Copy(source, destination, true);
-            txtOutput.AppendText("Copiado a: " + destination + "\r\n");
-        }
-
-        private void ExecuteCommand(string command, string workingDirectory)
-        {
-            ProcessStartInfo psi = new ProcessStartInfo
-            {
-                FileName = "cmd.exe",
-                Arguments = command,
-                WorkingDirectory = workingDirectory,
-                CreateNoWindow = true,
-                UseShellExecute = false,
-                RedirectStandardOutput = true
-            };
-
-            using (Process p = Process.Start(psi))
-            {
-                p.WaitForExit();
-                txtOutput.AppendText(p.StandardOutput.ReadToEnd());
-            }
-        }
-
-        [STAThread]
-        static void Main()
-        {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
-        }
-    }
-}
+:: Sección de finalización del script
+echo ------------------------------------------------------------
+echo Proceso completado.
+echo Presione una tecla para salir.
+pause > NUL
